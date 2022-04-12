71393cb445493237c7f54fa99196286e270b7437
NTNhanPS23291 <nhanntps23291@fpt.edu.vn>
2022-04-13 05:08:20 +0700

    Added form

--- js/tonghop.js
+++ js/tonghop.js
@@ -2,6 +2,7 @@ const goodsContainer = document.querySelector('.goods-container');
 const moveToCartPage = document.querySelector('.goods-cart--img');
 const goodsCart = document.querySelector('.goods-cart');
 const shopping = document.getElementById('shopping');
+const signupBtn = document.getElementById('signup');
 class Phone {
     constructor(phoneName, price, limit, quantity) {
         this.phoneName = phoneName;
@@ -65,10 +66,46 @@ function scrollToShop() {
     })
 }
 
+// function showForm() {
+//     if (form.classList.contains('form-hide')) form.classList.remove('form-hide');
+//     document.querySelectorAll('.blurred').forEach(elm => {
+//         elm.classList.add('on');
+//     })
+// }
+const form = document.querySelector('.container');
+// const input = document.querySelector('input');
+// input.list
+document.addEventListener('click', function (e) {
+    let isClickInside = form.contains(e.target);
+    if (form.classList.contains('form-hide') && e.target.id === 'signup') {
+        form.classList.remove('form-hide');
+        document.querySelectorAll('.blurred').forEach(elm => {
+            elm.classList.add('on');
+        })
+        return;
+    }
+
+    if (!form.classList.contains('form-hide') && !isClickInside) {
+        form.classList.add('form-hide');
+        document.querySelectorAll('.blurred').forEach(elm => {
+            elm.classList.remove('on');
+        })
+    }
+
+})
+
 shopping.addEventListener('click', scrollToShop);
+// signupBtn.addEventListener('click', showForm);
+
 
 moveToCartPage.addEventListener('click', () => {
-    window.open(`${window.location.href}giohang.html`, '_blank');
+    let url = window.location.href.match(/(index.html)/g);
+    if (url === null) {
+        window.open(`${window.location.href}giohang.html`, '_blank');
+    }
+    else {
+        window.open(`../giohang.html`, '_blank');
+    }
 });
 
 goodsContainer.addEventListener('input', function (e) {
@@ -82,7 +119,7 @@ goodsContainer.addEventListener('click', e => {
         addToCart(e.target);
         setCartQuantity();
     }
-})
+});
 
 window.addEventListener('storage', e => {
     if (e.url.match(/(giohang.html)/g) === null) return;
@@ -90,4 +127,63 @@ window.addEventListener('storage', e => {
     const newCart = JSON.parse(changedSession);
     cart = [...newCart];
     setCartQuantity();
-})
+});
+
+// window.addEventListener('click', function (e) {
+//     if (!e.target.classList.contains('container')) {
+//         document.querySelectorAll('.on').forEach(elm => elm.classList.remove('on'));
+//     }
+// })
+
+!function () {
+    'use strict';
+    angular.module('FormCheck', [])
+        .controller('FormCheckController', FormCheckController);
+    FormCheckController.$inject = ['$scope'];
+    function FormCheckController(s) {
+        s.info = {};
+        s.formValid = false;
+
+        s.switch = function () {
+            s.formValid = false;
+            s.blurClass = false;
+        }
+
+        s.formCheck = function ($event) {
+            $event.preventDefault();
+            let stdCode = s.info.mssv;
+            let stdName = s.info.name;
+            let stdEmail = s.info.email;
+            let checkEmail = !stdEmail ? null : stdEmail.match(/\w+@\w+(\.\w+){1,2}/g);
+            let stdGender = s.info.gender;
+            let stdHobbies = s.info.hobby;
+            let stdNation = s.info.nationality;
+            if (!stdCode || !stdName || !stdGender || !stdHobbies || !stdNation || +stdNation === 0 || !stdEmail) {
+                alert('Nhập thông tin chưa đúng. Mời nhập lại');
+                s.formValid = false;
+                return false;
+            }
+            if (stdEmail) {
+                if (checkEmail === null || checkEmail?.length !== 1 || checkEmail[0] !== stdEmail) {
+                    alert('Nhập thông tin chưa đúng. Mời nhập lại');
+                    s.formValid = false;
+                    return false;
+                }
+            }
+            if (stdHobbies) {
+                let hobbyArr = Object.values(stdHobbies);
+                if (hobbyArr === null || !hobbyArr.includes(true)) {
+                    alert('Nhập thông tin chưa đúng. Mời nhập lại');
+                    s.formValid = false;
+                    return false;
+                }
+            }
+            for (let info in s.info) {
+                delete s.info[info];
+            }
+            s.formValid = true;
+            s.blurClass = "blur";
+            return true;
+        }
+    }
+}();
